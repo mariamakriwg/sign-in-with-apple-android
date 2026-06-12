5a463e4dde725f8c34ed5c1405d5597fbfeadcca
mariamakriwg <mariamakriwg@gmail.com>
2026-06-12 15:01:30 +0300

    Update SignInWebViewDialogFragment.kt

--- signinwithapplebutton/src/main/java/com/willowtreeapps/signinwithapplebutton/view/SignInWebViewDialogFragment.kt
+++ signinwithapplebutton/src/main/java/com/willowtreeapps/signinwithapplebutton/view/SignInWebViewDialogFragment.kt
@@ -66,40 +66,24 @@ internal class SignInWebViewDialogFragment : DialogFragment() {
                 domStorageEnabled = true 
                 setSupportMultipleWindows(true)
 
+                          val finalUa = WebSettings.getDefaultUserAgent(this@WebViewActivity)
+                .replace("; wv", "")
+                .replace(" Version/4.0", "")
+            userAgentString = finalUa
 
-                userAgentString = "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.230 Mobile Safari/537.36"
             }
         }
+
+                CookieManager.getInstance().apply {
+    setAcceptCookie(true)
+    setAcceptThirdPartyCookies(webView, true)
+}
 
         // Adding a JavaScript interface
         val formInterceptorInterface = FormInterceptorInterface(authenticationAttempt.state, ::onCallback)
         webView.addJavascriptInterface(formInterceptorInterface, FormInterceptorInterface.NAME)
 
-        CookieManager.getInstance().apply {
-    setAcceptCookie(true)
-    setAcceptThirdPartyCookies(webView, true)
-}
 
-webView.webChromeClient = object : WebChromeClient() {
-    override fun onCreateWindow(
-        view: WebView,
-        isDialog: Boolean,
-        isUserGesture: Boolean,
-        resultMsg: Message
-    ): Boolean {
-        val newWebView = WebView(requireContext()).apply {
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            settings.userAgentString = view.settings.userAgentString
-        }
-        (resultMsg.obj as WebView.WebViewTransport).webView = newWebView
-        resultMsg.sendToTarget()
-        return true
-    }
-}
-
-
-        
         webView.webViewClient =
             UrlInterceptorWebViewClient(authenticationAttempt.redirectUri, FormInterceptorInterface.JS_TO_INJECT)
 
